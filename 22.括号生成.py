539d693e452f700f62f80102ac27502d25dc0245
ke <ke.guo@visualbehavior.ai>
2023-04-11 09:02:43 +0200

    question 22

---
+++ 22.括号生成.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode.cn id=22 lang=python3
+#
+# [22] 括号生成
+#
+
+# @lc code=start
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        def generate(A):
+            if len(A) == 2*n:
+                if valid(A):
+                    ans.append("".join(A))
+            else:
+                A.append('(')
+                generate(A)
+                A.pop()
+                A.append(')')
+                generate(A)
+                A.pop()
+
+        def valid(A):
+            bal = 0
+            for c in A:
+                if c == '(': bal += 1
+                else: bal -= 1
+                if bal < 0: return False
+            return bal == 0
+
+        ans = []
+        generate([])
+        return ans
+    # @lc code=end
